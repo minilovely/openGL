b91e11a5518be645ab4ff6944c9d1d2d2edafc1f
mini lovely <rsp2282493@163.com>
2025-08-14 17:11:06 +0800

    新建bezier文件，添加openGL基本的代码模板，关键词为openGLFramework，可自定义class命名

---
+++ test/bezier.cpp
@@ -0,0 +1,67 @@
+﻿#include<sb7.h>
+#include<string>
+#include<vmath.h>
+#include"myOpenGLErrorCheck.h"
+
+class bezier : public sb7::application
+{
+public:
+    bezier();
+    ~bezier();
+
+    void init()
+    {
+        sb7::application::init();
+        GLchar title[] = { "" };
+        memcpy(info.title, title, sizeof(title));
+    }
+    void startup();
+    void render(double time);
+    void shutdown();
+
+private:
+    GLuint vao;
+    GLuint buffer;
+    GLuint program;
+};
+DECLARE_MAIN(bezier);
+
+void bezier::startup()
+{
+    std::string ver_src =
+        R"(
+            
+        )";
+    std::string tcs_src =
+        R"(
+            
+        )";
+    std::string tes_src =
+        R"(
+            
+        )";
+    std::string frg_src =
+        R"(
+            
+        )";
+
+    Error::set_shader_source(ver_src, frg_src, tcs_src, tes_src);
+    program = Error::compile_program();
+    glUseProgram(program);
+}
+
+void bezier::render(double time)
+{
+}
+
+void bezier::shutdown()
+{
+}
+
+bezier::bezier()
+{
+}
+
+bezier::~bezier()
+{
+}
